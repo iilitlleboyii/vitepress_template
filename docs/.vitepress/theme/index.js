74bfbce2a8e552a09fa1bd0cea580d9d2fb7920d
iilitlleboyii <405422114@qq.com>
2023-09-27 23:11:34 +0800

    perf: 优化头部样式

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,8 @@
 import DefaultTheme from "vitepress/theme"
-import "./styles/vars.css"
+import "./styles/index.css"
 import "vitepress-theme-demoblock/dist/theme/styles/index.css"
 import { useComponents } from "./useComponents"
-import 'element-plus/dist/index.css'
+import "element-plus/dist/index.css"
 import ElementPlus from "element-plus"
 import zhCn from "element-plus/dist/locale/zh-cn.mjs"
 
